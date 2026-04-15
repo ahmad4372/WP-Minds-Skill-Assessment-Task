709000b1c8323ab40b7251460abf96918a7f38aa
Ahmad <mas7897860@gmail.com>
2026-04-15 20:37:06 +0500

    first commit

---
+++ wp-minds-skill-accessment-task.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WP Minds Skill Assessment Task
+ * Plugin URI:      https://github.com/ahmad4372/WP-Minds-Skill-Assessment-Task
+ * Description:     Custom WordPress plugin to manage reviews with CPT, meta fields, and a frontend slider with shortcode support.
+ * Author:          Muhammad Ahmad
+ * Author URI:      https://github.com/ahmad4372/
+ * Text Domain:     wp-minds-skill-assessment-task
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Minds_Skill_Assessment_Task
+ */
+
+// Your code starts here.
